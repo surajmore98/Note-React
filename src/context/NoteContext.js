1a1fb3eb878744da370fe4b0d4c8e62ecb34b9bc
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-05-16 16:12:43 +0530

    #feat - context added.

--- src/context/NoteContext.js
+++ src/context/NoteContext.js
@@ -5,9 +5,17 @@ const NoteContext = React.createContext();
 export const useNote = () => React.useContext(NoteContext);
 
 export const NoteProvider = ({children}) => {
-    const [loading, setLoading] = useState(false);
+    const [notes, setNotes] = useState([]);
+    const [archives, setArchives]  = useState([]);
+    const [trash, setTrash]  = useState([]);
     
-    const data = {loading, setLoading }
+    const [currentNote, setCurrentNote] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    
+    const data = { notes, setNotes, archives, setArchives,
+         trash, setTrash, currentNote, setCurrentNote,
+         loading, setLoading, searchText, setSearchText }
 
     return (
         <NoteContext.Provider value={data}>
