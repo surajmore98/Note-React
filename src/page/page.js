35458637c2f2e502d771281edd7584f15dd9c464
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-05-18 21:19:22 +0530

    #feat - route added.

--- src/page/page.js
+++ src/page/page.js
@@ -1,6 +1,10 @@
+import { Archive } from "./Archive";
 import { Home } from "./Home";
+import { Labels } from "./Labels";
 import { Login } from "./Login";
 import { Register } from "./Register";
 import { Logout } from "./Logout";
+import { Trash } from "./Trash";
+import { Notes } from "./Notes";
 
-export { Home, Login, Register, Logout }
+export { Archive, Home, Labels, Login, Register, Logout, Trash, Notes }
